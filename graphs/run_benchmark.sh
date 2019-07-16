a9566730b14ff8eca445aa9ed15557019c004330
c0ver <lgd84719@gmail.com>
2019-07-16 09:54:06 -0400

    finished graphs

--- graphs/run_benchmark.sh
+++ graphs/run_benchmark.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DIR='/media/hirwo/extra/pyBedGraph'
+
 # human
 files=(ENCFF321FZQ ENCFF050CCI ENCFF847JMY ENCFF631HEX)
 for file in ${files[*]}
 do
-	python3 make_benchmarks.py "../pyBedGraph/data/chrom_sizes/hg38.chrom.sizes" "../pyBedGraph/data/$file.bedGraph" "../pyBedGraph/data/$file.bigWig"
+	python3 make_benchmarks.py "$DIR/chrom_sizes/hg38.chrom.sizes" "$DIR/$file.bedGraph" "$DIR/$file.bigWig"
 	if [[ $? -ne 0 ]];
 	then
 	    exit 1
@@ -15,7 +17,7 @@ done
 files=(ENCFF376VCU ENCFF643WMY ENCFF384CMP ENCFF770CQD)
 for file in ${files[*]}
 do
-	python3 make_benchmarks.py "../pyBedGraph/data/chrom_sizes/mm10.chrom.sizes" "../pyBedGraph/data/$file.bedGraph" "../pyBedGraph/data/$file.bigWig"
+	python3 make_benchmarks.py "$DIR/chrom_sizes/mm10.chrom.sizes" "$DIR/$file.bedGraph" "$DIR/$file.bigWig"
 	if [[ $? -ne 0 ]];
 	then
 	    exit 1
